5217f6f008b27d2f6dd91bf26058553683da9bef
ismail karakaş <43730835+isml@users.noreply.github.com>
2021-05-31 21:48:32 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ def response():
     query = dict(request.form)['query']
     modelUrls = query.split("+linkdivider+")
     modelUrl = "https://firebasestorage.googleapis.com/v0/b/dismo-45c00.appspot.com/o/soner.stl?alt=media&token=e97408ce-0253-46c3-b755-72024bb4a1d2"
-    r1 = requests.get(url1, allow_redirects=True)
+    r1 = requests.get(modelUrl, allow_redirects=True)
     open('model1.stl', 'wb').write(r1.content)
     modelPath = "deneme/model1.glb"
 
