1e57b5211d49404cc9762c493133b709e40f47fc
ismail karakaş <43730835+isml@users.noreply.github.com>
2021-05-31 22:02:30 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -19,18 +19,36 @@ def response():
     r1 = requests.get(modelUrl, allow_redirects=True)
     open('model1.stl', 'wb').write(r1.content)
     modelPath = "deneme/model1.glb"
-    # firebasedeneme start
-    config = {
 
-      "apiKey": "AIzaSyB1nw436MIG5oq53Bd7_xYanYwA1U7GnH0",
-      "authDomain": "dismo-45c00.firebaseapp.com",
-      "projectId": "dismo-45c00",
-      "storageBucket": "dismo-45c00.appspot.com",
-      "serviceAccount": "serviceAcoountKey.json"
-      }
-    firebase_storage = pyrebase.initialize_app(config)
-    storege = firebase_storage.storage()
-    storege.child(modelPath).put("model1.stl")
+
+
+  
+    
+
+    path_to_stl = "model1.stl"
+    out_path = "model1.glb"
+    if len(sys.argv) > 3:
+      is_binary = True
+    else:
+      is_binary = False
+
+    if out_path.lower().endswith(".glb"):
+      print("Use binary mode since output file has glb extension")
+      is_binary = True
+    else:
+      if is_binary:
+        print("output file should have glb extension but not %s", out_path)
+
+    if not os.path.exists(path_to_stl):
+      print("stl file does not exists %s" % path_to_stl)
+
+    if not is_binary:
+      if not os.path.isdir(out_path):
+        os.mkdir(out_path)
+
+    stl_to_gltf(path_to_stl, out_path, is_binary)
+    # firebasedeneme start
+    
 
     # firebasedeneme finish
 
