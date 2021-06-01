b86a123cb83dc5e7b88625611a25d49abbcc3202
ismail karakaş <43730835+isml@users.noreply.github.com>
2021-06-01 19:39:21 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,33 +9,6 @@ app = Flask(__name__);
 
 
 @app.route("/bot", methods=["POST"])
-
-def response():
-
-
-    query = dict(request.form)['query']
-    modelUrls = query.split("+linkdivider+")
-    modelUrl = "https://firebasestorage.googleapis.com/v0/b/dismo-45c00.appspot.com/o/soner.stl?alt=media&token=e97408ce-0253-46c3-b755-72024bb4a1d2"
-    r1 = requests.get(modelUrl, allow_redirects=True)
-    open('model1.stl', 'wb').write(r1.content)
-    modelPath = "deneme/model1.glb"
-    
-    
-
-    path_to_stl = "soner.stl"
-    
-    is_binary=false
-    stl_to_gltf(path_to_stl, out_path, is_binary)
-    
-   
-    
-
-
-
-
-    resPuan = str(5)
-
-    return jsonify({"response": "1"})
 def stl_to_gltf(binary_stl_path, out_path, is_binary):
     import struct
 
@@ -284,6 +257,71 @@ def stl_to_gltf(binary_stl_path, out_path, is_binary):
             out.write(gltf2)
 
     print("Done! Exported to %s" %out_path)
+def response():
+
+
+    query = dict(request.form)['query']
+    modelUrls = query.split("+linkdivider+")
+    modelUrl = "https://firebasestorage.googleapis.com/v0/b/dismo-45c00.appspot.com/o/soner.stl?alt=media&token=e97408ce-0253-46c3-b755-72024bb4a1d2"
+    r1 = requests.get(modelUrl, allow_redirects=True)
+    open('model1.stl', 'wb').write(r1.content)
+    modelPath = "deneme/model1.glb"
+    
+    
+
+    
+    
+    import sys
+
+
+
+    path_to_stl = "model1.stl"
+    out_path = "model1.glb"
+    if len(sys.argv) > 3:
+        is_binary = True
+    else:
+        is_binary = False
+
+    if out_path.lower().endswith(".glb"):
+        print("Use binary mode since output file has glb extension")
+        is_binary = True
+    else:
+        if is_binary:
+            print("output file should have glb extension but not %s", out_path)
+
+    if not os.path.exists(path_to_stl):
+        print("stl file does not exists %s" % path_to_stl)
+
+    if not is_binary:
+        if not os.path.isdir(out_path):
+            os.mkdir(out_path)
+
+    print(is_binary)
+    stl_to_gltf(path_to_stl, out_path, is_binary)
+    config = {
+        "apiKey": "AIzaSyB1nw436MIG5oq53Bd7_xYanYwA1U7GnH0",
+        "authDomain": "dismo-45c00.firebaseapp.com",
+        "databaseURL": "https://dismo-45c00-default-rtdb.firebaseio.com/",
+        "projectId": "dismo-45c00",
+        "storageBucket": "dismo-45c00.appspot.com",
+        "messagingSenderId": "400086979067",
+        "appId": "1:400086979067:web:17d86fc6f7451d1bf63b5e",
+        "measurementId": "G-9DVNRGDQFJ"
+    }
+    firebase = pyrebase.initialize_app(config)
+    storage = firebase.storage()
+    storage.child("model1.glb").put("model1.glb")
+    
+   
+    
+
+
+
+
+    resPuan = str(5)
+
+    return jsonify({"response": "1"})
+
 
 
 
