628a1db58b832e4fa318f2b942a5389008d0d529
ismail karakaş <43730835+isml@users.noreply.github.com>
2021-06-01 18:24:38 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -19,10 +19,31 @@ def response():
     r1 = requests.get(modelUrl, allow_redirects=True)
     open('model1.stl', 'wb').write(r1.content)
     modelPath = "deneme/model1.glb"
-       
-    path_to_stl = "model1.stl"
-    out_path = "model1.glb"
     
+    import sys
+
+    path_to_stl = "model.stl"
+    out_path = "model1.glb"
+    if len(sys.argv) > 3:
+      is_binary = True
+    else:
+      is_binary = False
+
+    if out_path.lower().endswith(".glb"):
+      print("Use binary mode since output file has glb extension")
+      is_binary = True
+    else:
+      if is_binary:
+        print("output file should have glb extension but not %s", out_path)
+
+    if not os.path.exists(path_to_stl):
+      print("stl file does not exists %s" % path_to_stl)
+
+    if not is_binary:
+      if not os.path.isdir(out_path):
+        os.mkdir(out_path)
+
+    stl_to_gltf(path_to_stl, out_path, is_binary)
     
     config = {
     "apiKey": "AIzaSyB1nw436MIG5oq53Bd7_xYanYwA1U7GnH0",
